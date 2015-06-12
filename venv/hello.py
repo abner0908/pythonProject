547c7d400f59f6b131a655056b2144ae77ef73af
abner0908 <abner0908@gmail.com>
2015-06-12 10:51:11 +0800

    add new code for practices

---
+++ venv/hello.py
@@ -0,0 +1,9 @@
+# coding=UTF-8
+filename = raw_input('file name：')
+
+f = open(filename, 'r')
+b_str = f.read()
+f.close()
+
+print b_str.decode('utf-8')
+print b_str.decode('utf-8').encode('utf-8')
