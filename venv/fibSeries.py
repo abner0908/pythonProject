73dd7ba0959cece12699bf79dabe4176b9dfbf69
abner0908 <abner0908@gmail.com>
2015-06-18 12:21:40 +0800

    modify fibSeries

--- venv/fibSeries.py
+++ venv/fibSeries.py
@@ -5,5 +5,11 @@ def fib(max):
 		print n1
 		n1, n2 = n2, n1 + n2
 
-max = input('List a fibonacci series up to : ')
-fib(max)
+if __name__ == "__main__":
+	import sys
+	if len(sys.argv) > 1:
+		max = int(sys.argv[1])
+	else:
+		max = input('List a fibonacci series up to : ')
+
+	fib(max)
