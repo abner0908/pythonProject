bedfdd95b39deaa97bc565f05314870aefcd0b3b
abner0908 <abner0908@gmail.com>
2015-06-21 09:32:06 +0800

    Deleted files manually

--- venv/fibSeries.py
+++
@@ -1,9 +0,0 @@
-# coding=utf-8
-def fib(max):
-	n1, n2 = 1, 1
-	while n1 < max:
-		print n1
-		n1, n2 = n2, n1 + n2
-
-max = input('List a fibonacci series up to : ')
-fib(max)
--- venv/hello.py
+++
@@ -1,9 +0,0 @@
-# coding=UTF-8
-filename = raw_input('file name：')
-
-f = open(filename, 'r')
-b_str = f.read()
-f.close()
-
-print b_str.decode('utf-8')
-print b_str.decode('utf-8').encode('utf-8')
